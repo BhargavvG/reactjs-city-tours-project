862c2d2e74be31974d81cbd45b99903de4bc6da8
Bhargav <bhargav.g.912@gmail.com>
2021-12-25 20:43:10 +0530

    city-tours-project

---
+++ src/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "https://www.roadtraffic-technology.com/wp-content/uploads/sites/17/2017/10/1-image-18.jpg",
+    name: "new york bridge tour",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "https://media.tacdn.com/media/attractions-splice-spp-674x446/09/ee/94/ca.jpg",
+    name: "paris lights tour",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "https://media.tatler.com/photos/6141e967e00e3d8849bff273/master/w_1600%2Cc_limit/shutterstock_668643088-landscape.jpg",
+    name: "london royal palace tour",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "https://cdn.theculturetrip.com/wp-content/uploads/2017/09/central_road_kabukicho-sinjyuku-tokyo-1.jpg",
+    name: "tokyo sushi tour",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi",
+  },
+];
